269258a115777191ddbf7cd5c8dff4f0c2e5d70a
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-10-26 17:42:59 +0800

    Project setup and initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TodoList extends Component {
+	render() {
+		return (
+			<div>
+				<h1>TODOLIST</h1>
+			</div>
+		);
+	}
+}
+
+export default TodoList;
